e3072e9d5ca49d00aa052dc3c27a07bd1fa38aee
Teague <teague.omara@selu.edu>
2021-02-23 19:55:57 -0600

    AggMark+

--- script/DataAccess.R
+++ script/DataAccess.R
@@ -8,3 +8,7 @@ sampleInfo <- read_sheet("https://docs.google.com/spreadsheets/d/1yLeZAETb_u4z72
 demos <- read_sheet("https://docs.google.com/spreadsheets/d/13jCF6xD0UBmJJ08J2d6z-4xHEV2_L5NY91MaNBlppuQ/edit?usp=sharing", sheet = "ID & Demo")
 reproState <- read_sheet("https://docs.google.com/spreadsheets/d/13jCF6xD0UBmJJ08J2d6z-4xHEV2_L5NY91MaNBlppuQ/edit?usp=sharing", sheet = "IDs Repro Stage")
 sampleWeek <- read_sheet("https://docs.google.com/spreadsheets/d/13jCF6xD0UBmJJ08J2d6z-4xHEV2_L5NY91MaNBlppuQ/edit?usp=sharing", sheet = "fecal study weeks")
+
+aggMark <- read_sheet("https://docs.google.com/spreadsheets/d/1WHsp4nW_mVVLoLeXI5QjyeAOiHeoShu5Vqbuj-ic9xw/edit?usp=sharing")
+
+
